6a4847991d7c3d313cc3da9e944f84bc950b6846
Lucas Santos <lucas201273@gmail.com>
2024-07-13 15:35:46 -0300

    chore: initial commit

---
+++ src/main/java/br/com/plannermanager/PlannermanagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.plannermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannermanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlannermanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/plannermanager/PlannermanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.plannermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannermanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
