3f984182d253e4b0ddf70d174ed8a7cf5754be74
Lucas Santos <lucas201273@gmail.com>
2024-07-13 19:03:00 -0300

    feat: create project manager exception handler

---
+++ src/main/java/br/com/plannermanager/controllers/exception/ProjectManagerExceptionHandler.java
@@ -0,0 +1,37 @@
+package br.com.plannermanager.controllers.exception;
+
+
+import br.com.plannermanager.bunisses.exceptions.EntityAlreadyExistsException;
+import br.com.plannermanager.dto.response.StandardErrorPayload;
+import jakarta.servlet.http.HttpServletRequest;
+import org.aspectj.weaver.ast.IExprVisitor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+@ControllerAdvice
+public class ProjectManagerExceptionHandler {
+
+  @ExceptionHandler(EntityAlreadyExistsException.class)
+  public ResponseEntity<StandardErrorPayload> entityAlreadyExists(HttpServletRequest httpServletRequest, EntityAlreadyExistsException exception){
+    String pathUrl = httpServletRequest.getRequestURI();
+    int badRequestStatusCode = HttpStatus.BAD_REQUEST.value();
+    StandardErrorPayload responseData = StandardErrorPayload
+              .builder()
+            .error("Entity already exists!")
+            .exceptionMessage(exception.getMessage())
+            .path(pathUrl)
+            .status(badRequestStatusCode)
+            .timestamp(LocalDateTime.now())
+            .fieldError(new ArrayList<>())
+            .build();
+    return ResponseEntity.status(badRequestStatusCode).body(responseData);
+  }
+
+
+
+}
---
+++ src/main/java/br/com/plannermanager/dto/response/StandardErrorPayload.java
@@ -0,0 +1,28 @@
+package br.com.plannermanager.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class StandardErrorPayload implements Serializable {
+
+    private LocalDateTime timestamp;
+    private Integer status;
+    private String error;
+    private String path;
+    @JsonProperty("exception_message")
+    private String exceptionMessage;
+    @JsonProperty("field_errors")
+    private List<Object> fieldError;
+
+}
